fbe96c6ae9f0f21ca368f38ecfe08d22e47cfc36
Takuma Kira <takuma@kirakiraworx.com>
2023-11-27 11:10:04 +0900

    [dev] dotnet aspnet-codegenerator controller -name NotesController -async -api -m Note -dc NotesContext -outDir Controllers

---
+++ Controllers/NotesController.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NoteTakingAppApiDotnet.Models;
+
+namespace NoteTakingAppApiDotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly NotesContext _context;
+
+        public NotesController(NotesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Notes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Note>>> GetNote()
+        {
+          if (_context.Note == null)
+          {
+              return NotFound();
+          }
+            return await _context.Note.ToListAsync();
+        }
+
+        // GET: api/Notes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Note>> GetNote(long id)
+        {
+          if (_context.Note == null)
+          {
+              return NotFound();
+          }
+            var note = await _context.Note.FindAsync(id);
+
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            return note;
+        }
+
+        // PUT: api/Notes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNote(long id, Note note)
+        {
+            if (id != note.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(note).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NoteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Notes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Note>> PostNote(Note note)
+        {
+          if (_context.Note == null)
+          {
+              return Problem("Entity set 'NotesContext.Note'  is null.");
+          }
+            _context.Note.Add(note);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNote", new { id = note.Id }, note);
+        }
+
+        // DELETE: api/Notes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNote(long id)
+        {
+            if (_context.Note == null)
+            {
+                return NotFound();
+            }
+            var note = await _context.Note.FindAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+
+            _context.Note.Remove(note);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool NoteExists(long id)
+        {
+            return (_context.Note?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}

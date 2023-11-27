8c993ade4660171777c8c0e6e55a3973f586fa15
Takuma Kira <takuma@kirakiraworx.com>
2023-11-27 10:59:06 +0900

    [dev] Add a database context

---
+++ Models/NotesContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NoteTakingAppApiDotnet.Models;
+
+public class NotesContext : DbContext
+{
+  public NotesContext(DbContextOptions<NotesContext> options)
+    : base(options)
+  {
+  }
+
+  public DbSet<Note> Note { get; set; } = null!;
+}
